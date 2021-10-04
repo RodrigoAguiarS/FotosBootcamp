f4a5249bb65b93ab1376f0d21705afc0dd802fa1
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-10-04 11:21:24 -0300

    Conceitos iniciais e preparação do projeto - Recurso de Câmera

--- app/src/main/java/com/rodrigo/fotosbootcamp/MainActivity.kt
+++ app/src/main/java/com/rodrigo/fotosbootcamp/MainActivity.kt
@@ -11,6 +11,11 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private val PERMISSION_CODE_IMAGE_PICK = 1000
+        private val IMAGE_PICK_CODE = 1001
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,7 +25,7 @@ class MainActivity : AppCompatActivity() {
                 if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
                 == PackageManager.PERMISSION_DENIED ){
                     val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-                    requestPermissions(permission, PERMISSION_CODE)
+                    requestPermissions(permission, PERMISSION_CODE_IMAGE_PICK)
                 }
                 else{
                     pickImageFromGaley()
@@ -36,11 +41,11 @@ class MainActivity : AppCompatActivity() {
     private fun pickImageFromGaley() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_PICk_CODE)
+        startActivityForResult(intent, IMAGE_PICK_CODE)
     }
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         when (requestCode){
-            PERMISSION_CODE -> {
+            PERMISSION_CODE_IMAGE_PICK -> {
                 if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                     pickImageFromGaley()
                 }
@@ -54,12 +59,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICk_CODE){
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
             image_view.setImageURI(data?.data)
         }
-    }
-    companion object{
-        private val PERMISSION_CODE = 1000
-        private val IMAGE_PICk_CODE = 1001
     }
 }
